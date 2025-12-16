11e0b78b7b3f3a32ff5ccbde8d0c4891bdcfce3f
lihs-ie <lihs.dev@gmail.com>
2025-12-16 20:03:16 +0900

    4章まで

---
+++ adapter/src/database/mod.rs
@@ -0,0 +1,25 @@
+use shared::config::DatabaseConfig;
+use sqlx::{PgPool, postgres::PgConnectOptions};
+
+fn make_pg_connect_options(config: &DatabaseConfig) -> PgConnectOptions {
+    PgConnectOptions::new()
+        .host(&config.host)
+        .port(config.port)
+        .username(&config.username)
+        .password(&config.password)
+        .database(&config.database)
+}
+
+#[derive(Clone)]
+pub struct ConnectionPool(PgPool);
+
+impl ConnectionPool {
+    // sqlx::PgPool への参照を取得する
+    pub fn inner_ref(&self) -> &PgPool {
+        &self.0
+    }
+}
+
+pub fn connect_database_with(config: &DatabaseConfig) -> ConnectionPool {
+    ConnectionPool(PgPool::connect_lazy_with(make_pg_connect_options(config)))
+}
---
+++ src/bin/app.rs
@@ -0,0 +1,34 @@
+use std::net::{Ipv4Addr, SocketAddr};
+
+use adapter::database::connect_database_with;
+use anyhow::{Error, Result};
+use api::route::health::build_health_routers;
+use axum::Router;
+use registry::AppRegistry;
+use tokio::net::TcpListener;
+
+use shared::config::AppConfig;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    bootstrap().await
+}
+
+async fn bootstrap() -> Result<()> {
+    let app_config = AppConfig::new()?;
+
+    let pool = connect_database_with(&app_config.database);
+
+    let registry = AppRegistry::new(pool);
+
+    let app = Router::new()
+        .merge(build_health_routers())
+        .with_state(registry);
+
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on {}", addr);
+
+    axum::serve(listener, app).await.map_err(Error::from)
+}
